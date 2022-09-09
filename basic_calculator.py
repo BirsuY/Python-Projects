d537ba53553aa7671b4b3cd355368dcf8f3afc5f
BirsuYilmaz <93135059+BirsuY@users.noreply.github.com>
2022-09-09 15:12:05 +0300

    Calculator
    A simple calculator that lets you do 5 simple equation between two number with or without a decimal point

---
+++ basic_calculator.py
@@ -0,0 +1,32 @@
+#birsuyilmaz
+def basic_calculator():
+    operator = input("Please enter the operator that you would like to use (+, -, /, //, *, %): ")
+    num1 = float(input("Please enter a num: "))
+    num2 = float(input("Please enter another num: "))
+
+
+    match operator:
+        case '+':
+            return num1 + num2
+        case '-':
+            return num1 - num2
+        case '/':
+            return num1 / num2
+        case '//':
+            return num1 // num2
+        case '*':
+            return num1 * num2
+        case '%':
+            return num1 % num2
+        case _:
+            return 'incomputable'
+
+print("Welcome to Luna Calculator\n")
+while True:
+    print("result:", basic_calculator())
+    option = input("If you would like to continiue, enter c\nIf you would like to exit, enter e: ")
+    if(option == 'e'):
+        break
+
+print("Thanks for using Luna Calculator\n")
+#birsuyilmaz
